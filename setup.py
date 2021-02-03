9b7c658143033eba2e10350af3d31071ff3b1d0a
rha <rha@dhigroup.com>
2021-02-03 09:46:27 +0100

    Add setup.py to enable package install

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="mikecloudio",
+    version='0.0.1',
+    install_requires=["pandas"],
+    url="https://github.com/rhaDHI/mikecloudio",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
