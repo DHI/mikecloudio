acae3e740402774169a50576556cad3fda63006e
rha <rha@dhigroup.com>
2021-02-04 20:20:33 +0100

    Create multidimensional data

--- mikecloudio/multidimensional.py
+++ mikecloudio/multidimensional.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 from mikecloudio.request import create_header, request
 
@@ -18,3 +19,52 @@ def get_multidimensional_dataset(connection, path, data_type='md'):
 
 def get_datetimes_from_dataset(dataset):
     return pd.to_datetime([time['v'] for time in dataset['temporalDomain']['times']])
+
+
+def request_multidimensional_data(connection, path, query, data_type='md'):
+    """
+
+    :param connection: Connection
+    :param path: dataset path, e.g. GHM/GHM_CHIRPS/GridData
+    :param query:
+    :param data_type: 'md' for multidimensional
+    :return:
+    """
+    dataset_id, project_id = connection.get_dataset_id_from_path(path, return_subproject_id=True)
+    header = create_header(connection.api_key,
+                           dhi_project_id=project_id,
+                           dhi_dataset_id=dataset_id,
+                           dhi_service_id=data_type,
+                           Content_Type='application/json',
+                           api_version="1")
+
+    command = f"api/{data_type}/{dataset_id}/query"
+    url = connection.url + command
+    response = requests.post(url, json=query, headers=header)
+    return response.json()
+
+
+def create_multidimensional_data_query(item_indices=None, time_range=None,
+                                       include_geometries=False,
+                                       include_values=True):
+
+    item_indices = [0] if item_indices is None else item_indices
+    time_range = [0, 10] if time_range is None else time_range
+
+    return {
+        "itemFilter": {
+            "itemIndices": item_indices
+        },
+        "spatialFilter": {
+            "geometry": "POINT (100.0983376133463 14.26718894186121)",
+            "srid": 4326
+        },
+        "temporalFilter": {
+            "type": "TemporalIndexFilter",
+            "from": time_range.min(),
+            "to": time_range.max()
+        },
+        "verticalFilter": None,
+        "includeGeometries": include_geometries,
+        "includeValues": include_values
+    }
