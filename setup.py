e94dfdf32916e317a1aa68f40e07545bb43972ea
rha <rha@dhigroup.com>
2021-02-08 11:33:24 +0100

    Add shapely to create geometry wkt strings

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="mikecloudio",
-    version='0.0.1',
-    install_requires=["pandas"],
+    version='0.0.2',
+    install_requires=["pandas", "shapely"],
     url="https://github.com/rhaDHI/mikecloudio",
     packages=setuptools.find_packages(),
     include_package_data=True,
